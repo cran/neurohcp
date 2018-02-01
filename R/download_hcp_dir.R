7d4442332e6403e607b8c4d81a1a207f9ce20a91
John Muschelli <muschellij2@gmail.com>
2018-02-01 20:09:46 +0000

    version 0.7.3

--- R/download_hcp_dir.R
+++ R/download_hcp_dir.R
@@ -1,6 +1,6 @@
 #' @title Download an entire directory from HCP
 #' @description Downloads a directory/folder from HCP database
-#' @param prefix Folder to downlaod
+#' @param prefix Folder to download
 #' @param delimiter Delimiter for files
 #' @param outdir Output directory
 #' @param verbose Should diagnostic values be printed?
@@ -14,7 +14,7 @@
 #' @examples
 #' if (have_aws_key()) {
 #'    prefix = "HCP/100307/release-notes"
-#'    res = download_hcp_dir(prefix = prefix)
+#'    res = download_hcp_dir(prefix = prefix, verbose = FALSE)
 #' }
 download_hcp_dir = function(
   prefix,
@@ -24,6 +24,7 @@ download_hcp_dir = function(
   ...) {
   ret = hcp_list_files(prefix = prefix,
                        delimiter = delimiter,
+                       verbose = verbose,
                        ...)
 
   res = parse_list_files(ret)
@@ -72,3 +73,13 @@ download_fcp_dir = function(
                     bucket = "fcp-indi",
                     sign = FALSE)
 }
+
+#' @export
+#' @rdname download_hcp_dir
+download_openneuro_dir = function(
+  ...) {
+  download_hcp_dir(...,
+                   bucket = "openneuro",
+                   sign = FALSE)
+}
+
